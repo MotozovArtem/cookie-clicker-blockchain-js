486fead79b1da3cb87e15ecce917d9d6d8eeda97
PaulR <ipashok97@gmail.com>
2018-09-27 14:44:55 +0300

    added Steroids button, not sure if it works

--- src/app/index.jsx
+++ src/app/index.jsx
@@ -1,11 +1,17 @@
 import React, {Component} from 'react';
 import ReactDom from 'react-dom';
+import {Alert} from "react-native"
+import {Button} from "reactstrap";
 
 class App extends Component {
     render() {
+        const showAlert = () => {Alert.alert("Motherfucking alert", "TS is just JS on steroids")};
         return (
             <div className="container">
                 <div className="row">
+                    <Button color="danger" onClick={showAlert}>
+                        Тык
+                    </Button>
                     <div className="col-sm-6">
                         <div className="input-group">
                             <input name="reciveAddr" className="form-control" type="text"/>
